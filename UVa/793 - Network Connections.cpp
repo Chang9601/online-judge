7b5a350cc5c8b716c8b4c064fa0dd1f07b45a4c5
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-05-03 21:22:49 +0900

    Update 793 - Network Connections.cpp

--- UVa/793 - Network Connections.cpp
+++ UVa/793 - Network Connections.cpp
@@ -1,11 +1,13 @@
 #include <cstdio>
 using namespace std;
-int t,n;
-struct Disjoint{
+int T;
+struct DSU{
     int p[100001],r[100001];
+
     void init(int n)
     {
-        for(int i = 1; i <= n; i++)
+        int i;
+        for(i = 1; i <= n; i++)
         {
             p[i] = i;
             r[i] = 1;
@@ -14,54 +16,80 @@ struct Disjoint{
 
     int find(int u)
     {
-        if(u == p[u]) return u;
+        if(u == p[u])
+            return u;
         return p[u] = find(p[u]);
     }
 
     int merge(int u,int v)
     {
         u = find(u); v = find(v);
-        if(u == v) return 0;
-        if(r[u] > r[v])
+
+        if(u == v)
+            return 0;
+
+        if(r[v] < r[u])
         {
-            p[v] = u;
             r[u] += r[v];
+            p[v] = u;
         }
         else
         {
-            p[u] = v;
             r[v] += r[u];
+            p[u] = v;
         }
-
         return 1;
     }
-
 };
+
+void deleteNewline(char str[])
+{
+    int i = 0;
+    while(str[i] != '\n')
+        i++;
+    str[i] = '\0';
+}
+
+void clearInputBuffer()
+{
+    while(getchar() != '\n')
+        ;
+}
 
 int main()
 {
-    scanf("%d",&t);
-    while(t--)
-    {   
+    scanf("%d",&T);
+    while(T--)
+    {
+        int n;
         scanf("%d",&n);
-        Disjoint a;
-        a.init(n);
-        getchar(); char str[100];
-        char c; int u,v; int s = 0, us = 0;
-        while(gets(str))
+        clearInputBuffer();
+        char str[100];
+        DSU d; d.init(n);
+        char c;
+        int u,v,s,us;
+        s = us = 0;
+
+        while(fgets(str,sizeof(str),stdin))
         {
-            if(str[0] == '\0') break;
+            deleteNewline(str);
+            if(str[0] == '\0')
+                break;
             sscanf(str,"%c %d %d",&c,&u,&v);
-            if(c == 'c') a.merge(u,v);
+            if(c == 'c')
+                d.merge(u,v);
             else
             {
-                int ret = a.find(v) == a.find(u);
-                if(ret == 1) s++;
-                else us++;
+                u = d.find(u); v = d.find(v);
+                if(u == v)
+                    s++;
+                else 
+                    us++;
             }
         }
         printf("%d,%d\n",s,us);
-        if(t) puts("");
+        if(T) 
+            puts("");
     }
-    return 0;
+    return 0;    
 }
