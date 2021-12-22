94c9bb652e8e525bbcaa8ea7b588f5f46b996797
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-12-22 15:20:37 +0900

    숫자 문자열과 영단어(자바) 해결

---
+++ Programmers/숫자 문자열과 영단어.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        String[] nums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+		int len = s.length();
+		int i = 0, j = 0, idx = 0;
+		int num = 0;
+		String sub = "";
+
+		while(i < len){
+			if(Character.isDigit(s.charAt(i))){
+				num = num*10+(s.charAt(i)-'0');
+				i++;
+			}else{
+                for(j = 0; j < nums.length; j++){
+					idx = s.indexOf(nums[j], i);
+					if(idx == i){
+						sub = nums[j];
+                        break;
+					}
+                }
+				num = num*10+j;    
+				i += sub.length();
+			}
+		}
+        return num;
+    }
+}
