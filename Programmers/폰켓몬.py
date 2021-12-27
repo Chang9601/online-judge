f0673a34b62a12f6a3fa07c96ae7d62d0b25b65d
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-12-27 15:58:27 +0900

    Update 폰켓몬.py

--- Programmers/폰켓몬.py
+++ Programmers/폰켓몬.py
@@ -1,1 +1,15 @@
+def solution(nums):
+    nums = sorted(nums)
+    ans = 0
+    size = len(nums)/2
 
+    print(nums)
+
+    last = -1
+    for n in nums:
+        if n != last:
+            last = n
+            if ans < size:
+                ans += 1
+
+    return ans
